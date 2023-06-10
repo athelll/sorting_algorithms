b12dbc65f31e6879a523ced9ad456fcaf0bf64aa
6felix <felix.okoronkwo.1@gmail.com>
2023-06-10 13:59:25 +0100

    Implemented Bubble Sort Algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,25 +6,25 @@
   */
 void bubble_sort(int *array, size_t size)
 {
-    bool swapped;
-    int buffer;
-    size_t n = size, a, b;
+	bool swapped;
+	int buffer;
+	size_t n = size, a, b;
 
-    for (a = 0; a < n; a++)
-    {
-        swapped = false;
-        for (b = 0; b < n - 1; b++)
-        {
-            if (array[b] > array[b + 1])
-            {
-                buffer = array[b];
-                array[b] = array[b + 1];
-                array[b + 1] = buffer;
-                swapped = true;
-                print_array(array, size);
-            }
-        }
-        if (swapped == true)
-        continue;
-    }
+	for (a = 0; a < n; a++)
+	{
+		swapped = false;
+		for (b = 0; b < n - 1; b++)
+		{
+			if (array[b] > array[b + 1])
+			{
+				buffer = array[b];
+				array[b] = array[b + 1];
+				array[b + 1] = buffer;
+				swapped = true;
+				print_array(array, size);
+			}
+	}
+	if (swapped == true)
+		continue;
+	}
 }
