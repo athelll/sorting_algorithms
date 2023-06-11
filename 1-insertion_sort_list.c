ac9fd9f7992debb4739b7b5c3f10ceb79ef132c0
6felix <felix.okoronkwo.1@gmail.com>
2023-06-11 12:20:31 +0100

    Insertion sort for doubly linked list 1.1: fails when pushing list to the head of the list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,4 +1,6 @@
 #include "sort.h"
+#include <stdlib.h>
+#include <stdio.h>
 /*
 *
 *
@@ -9,27 +11,30 @@ void insertion_sort_list(listint_t **list)
 	listint_t *buffer;
 	listint_t *prev;
 	listint_t *current = *list;
-
+	
 	while(current != NULL)
 	{
 		a = current->n;
 		prev = current->prev;
+
 		while (prev != NULL && prev->n > a)
 		{
-			current = prev->next;
 			/** buffer for prev ptrs **/
 			buffer->next = prev->next;
 			buffer->prev = prev->prev;
 			/** pushes prev forward **/
 			prev->next = current->next;
-			prev->prev = current->prev;
+			prev->prev = current;
 			/** pushes current backward **/
+			current->next->prev = prev;
 			current->next = prev;
+			// problematic
 			current->prev = buffer->prev;
 			current->prev->next = current;
 			print_list(*list);
 			prev = current->prev;
 		}
 		current = current->next;
+		printf("%p\n", current);
 	}
 }
