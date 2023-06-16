78d974a2f3c2b8223ca061893ca841dc5e99335e
6felix <felix.okoronkwo.1@gmail.com>
2023-06-16 01:15:37 +0100

    Implemented Cocktail-Shaker Sort Algorithm

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,20 +1,23 @@
 #include "sort.h"
-#include <stdio.h>
 
+/**
+ * node_swapper - swaps nodes in a Doubly-linked List
+ * @first: first node
+ * @second: second node
+ * @list: pointer to Doubly-linked list
+ */
 void node_swapper(listint_t *first, listint_t *second, listint_t **list)
 {
 	listint_t *buffer;
 
-  buffer = malloc(sizeof(listint_t));
+	buffer = malloc(sizeof(listint_t));
 	if (buffer == NULL)
 		return;
 
 	buffer->next = second->next;
 	buffer->prev = first->prev;
-
 	first->next = second->next;
 	first->prev = second;
-
 	second->next = first;
 	second->prev = buffer->prev;
 
@@ -30,61 +33,86 @@ void node_swapper(listint_t *first, listint_t *second, listint_t **list)
 	free(buffer);
 }
 
-listint_t* last_node(listint_t **list)
+/**
+ * forward_sorter - sortes list in forward direction; remembers metadata
+ * for each swaps: its swapped state and the last node that was swapped
+ *
+ * @current: node to begin sort from
+ * @list: ppointer to list
+ * Return: returns metadata that tells if a swap occured and at what node
+ * the last swap occured;
+ */
+swap_data forward_sorter(listint_t *current, listint_t **list)
 {
-	listint_t *node = *list;
+	swap_data swapped = {false, current};
 
-	do {
-		node = node->next;
-	} while (node->next);
-
-	return node;
-}
-
-bool forward_sorter(listint_t* current, listint_t **list)
-{
-	bool swapped = false;
-
-	while(current && current->next)
+	while (current && current->next)
 	{
 		if (current->n > current->next->n)
 		{
 			node_swapper(current, current->next, list);
-			swapped = true;
+			swapped.is_swapped = true;
+			swapped.current = current;
 			continue;
 		}
 		current = current->next;
 	}
-	return swapped;
+	return (swapped);
 }
 
-bool backward_sorter(listint_t* current, listint_t **list)
+/**
+ * backward_sorter - sortes list in backward direction; remembers metadata
+ * for each swaps: its swapped state and the last node that was swapped
+ *
+ * @current: node to begin sort from
+ * @list: ppointer to list
+ * Return: returns metadata that tells if a swap occured and at what node
+ * the last swap occured;
+ */
+swap_data backward_sorter(listint_t *current, listint_t **list)
 {
-	bool swapped = false;
+	swap_data swapped = {false, current};
 
-	while(current && current->prev)
+	while (current && current->prev)
 	{
 		if (current->n < current->prev->n)
 		{
 			node_swapper(current->prev, current, list);
-			swapped = true;
+			swapped.is_swapped = true;
+			swapped.current = current;
 			continue;
 		}
 		current = current->prev;
 	}
-	return swapped;
+	return (swapped);
 }
 
+/**
+ * cocktail_sort_list - Algorithm that applies cocktail sort
+ * a Doubly-linked list
+ *
+ * @list: pointer to Doubly-linked list
+ */
 void cocktail_sort_list(listint_t **list)
 {
 	bool swapped;
-	listint_t* head_node = *list;
-	listint_t* end_node = last_node(list);
+	swap_data swapped_data;
+	listint_t *head_node = *list;
+	listint_t *end_node;
+
+	if (!list || !(list->next))
+		return;
 
 	do {
-		swapped = forward_sorter(head_node, list);
+		swapped_data = forward_sorter(head_node, list);
+		swapped = swapped_data.is_swapped;
+
 		if (!swapped)
 			break;
-		swapped = backward_sorter(end_node, list);
+
+		end_node = swapped_data.current;
+		swapped_data = backward_sorter(end_node, list);
+		swapped = swapped_data.is_swapped;
+		head_node = swapped_data.current;
 	} while (swapped);
 }
--- sort.h
+++ sort.h
@@ -20,6 +20,12 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+typedef struct swap_data
+{
+	bool is_swapped;
+	listint_t* current;
+} swap_data;
+
 /** initial functions **/
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
@@ -32,14 +38,14 @@ void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 
-/** my function decalrations **/
+/** my custom function decalrations and datatypes **/
 void swap(int *array, int first, int second, size_t size);
 void sort_quick(int *array, int low, int high, size_t size);
 int partition(int *array, int low, int high, size_t size);
 /** cocktail_sort **/
 void node_swapper(listint_t *current, listint_t *prev, listint_t **list);
 listint_t* last_node(listint_t **list);
-bool forward_sorter(listint_t* current, listint_t **list);
-bool backward_sorter(listint_t* current, listint_t **list);
+swap_data forward_sorter(listint_t* current, listint_t **list);
+swap_data backward_sorter(listint_t* current, listint_t **list);
 
 #endif
